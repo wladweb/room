deee9482a62d12a892c1ae371d3be93bd83ecc69
wladweb <wladwebwork@gmail.com>
2020-01-31 14:04:09 +0300

    zooming

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public class Interact : MonoBehaviour
+{
+    private DisplayImage currentDisplay;
+
+    void Start()
+    {
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 rayStartPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(rayStartPosition, Vector2.zero, 100);
+
+            if (hit && hit.transform.CompareTag("Interactable"))
+            {
+                Debug.Log("Hit");
+                hit.transform.GetComponent<IInteractable>().Interact(currentDisplay);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ZoomInObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ZoomInObject : MonoBehaviour, IInteractable
+{
+    public float zoomRatio = .5f;
+    private float positionX;
+    private float positionY;
+    private float positionZ;
+
+    void SetUpCameraPosition() 
+    {
+        positionX = this.transform.position.x;
+        positionY = this.transform.position.y;
+        positionZ = Camera.main.transform.position.z;
+    }
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        SetUpCameraPosition();
+        Camera.main.orthographicSize *= zoomRatio;
+        Camera.main.transform.position = new Vector3(positionX, positionY, positionZ);
+    }
+}
