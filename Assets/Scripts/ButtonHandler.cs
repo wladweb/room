2ab5c041cb162bc235b8f9ce2a23acbdacd7c09a
wladweb <wladwebwork@gmail.com>
2020-01-31 12:10:49 +0300

    room navigation

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private DisplayImage currentDisplay;
+
+    private void Start()
+    {
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+    }
+
+    public void OnRightClickArrow()
+    {
+        currentDisplay.CurrentWall += 1;
+    }
+
+    public void OnLeftClickArrow()
+    {
+        currentDisplay.CurrentWall -= 1;
+    }
+}
---
+++ Assets/Scripts/DisplayImage.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class DisplayImage : MonoBehaviour
+{
+    private int _currentWall;
+    private int _previousWall;
+
+    private void Start()
+    {
+        _currentWall = 1;
+        _previousWall = 0;
+    }
+
+    private void Update()
+    {
+        if (_currentWall != _previousWall) 
+        {
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/wall" + CurrentWall);
+        }
+    }
+
+    public int CurrentWall 
+    {
+        get 
+        {
+            return _currentWall;
+        }
+        set
+        {
+            if (value == 5)
+            {
+                _currentWall = 1;
+            }
+            else if (value == 0)
+            {
+                _currentWall = 4;
+            }
+            else
+            {
+                _currentWall = value;
+            }
+
+            
+        }
+    }
+}
