1dc218b314d19a24eee164b382492dd2ce8e26ca
wladweb <wladwebwork@gmail.com>
2020-02-06 20:05:33 +0300

    add book slide & paper pickup

---
+++ Assets/Scripts/DynamicObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicObject : MonoBehaviour, IInteractable
+{
+    public string UnlockItem;
+    public GameObject ChangeStateSprite;
+    private GameObject inventory;
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        if (inventory.GetComponent<Inventory>().CurrentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem || UnlockItem == "")
+        {
+            ChangeStateSprite.SetActive(true);
+            inventory.GetComponent<Inventory>().CurrentSelectedSlot.GetComponent<Slot>().ClearSlot();
+        }
+    }
+
+    private void Start()
+    {
+        ChangeStateSprite.SetActive(false);
+        inventory = GameObject.Find("Inventory");
+    }
+}
