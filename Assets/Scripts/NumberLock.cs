d15ecfefbb74c59b81195f1f53d1b19c87693ac9
wladweb <wladwebwork@gmail.com>
2020-02-06 13:19:33 +0300

    changing numbers on NumberLocker

---
+++ Assets/Scripts/NumberLock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberLock : MonoBehaviour
+{
+    private string password;
+    public Sprite[] numberSprites;
+    public int[] currentIndividualIndex = new int[4] { 0, 0, 0, 0 };
+
+    private void Start()
+    {
+        LoadAllNumberSprites();
+    }
+
+    void LoadAllNumberSprites() 
+    {
+        numberSprites = Resources.LoadAll<Sprite>("Sprites/numbers");
+    }
+}
