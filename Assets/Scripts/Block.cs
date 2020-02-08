6a484deea77dedb11e5ca9ddcfa9633d922ea4df
wladweb <wladwebwork@gmail.com>
2020-02-08 17:19:35 +0300

    add scale ui

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Block : MonoBehaviour, IDragHandler, IDropHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Scale scale = GameObject.Find("Scale").GetComponent<Scale>();
+
+        for (int i = 0, l = scale.scaleBoxes.Length; i < l; i++) 
+        { 
+            //
+        }
+    }
+}
