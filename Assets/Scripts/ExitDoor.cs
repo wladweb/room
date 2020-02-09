a43267a1bbaa6311e963dbe47abf0304d2eb5e2a
wladweb <wladwebwork@gmail.com>
2020-02-09 17:37:19 +0300

    add win message

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitDoor : MonoBehaviour
+{
+    public string UnlockItem;
+    public GameObject ChangeStateSprite;
+    private GameObject inventory;
+    public GameObject EscapeMessage;
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        if (inventory.GetComponent<Inventory>().CurrentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem || UnlockItem == "")
+        {
+            ChangeStateSprite.SetActive(true);
+            gameObject.layer = 2;
+        }
+    }
+
+    private void Start()
+    {
+        ChangeStateSprite.SetActive(false);
+        inventory = GameObject.Find("Inventory");
+    }
+}
